a2ee5904cadd7e2d9f1d202d3d0a3e554d8a8fc0
roydon <3133010060@qq.com>
2023-05-18 16:31:59 +0800

    新闻修改和删除接口

--- community-business/community-business-news/src/main/java/com/roydon/business/news/controller/NewsController.java
+++ community-business/community-business-news/src/main/java/com/roydon/business/news/controller/NewsController.java
@@ -5,10 +5,12 @@ import com.roydon.business.news.service.AppNewsService;
 import com.roydon.common.annotation.Log;
 import com.roydon.common.core.controller.BaseController;
 import com.roydon.common.core.domain.AjaxResult;
+import com.roydon.common.core.domain.entity.SysUser;
 import com.roydon.common.core.page.TableDataInfo;
 import com.roydon.common.enums.BusinessType;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -50,6 +52,22 @@ public class NewsController extends BaseController {
     @PutMapping("/changeStatus")
     public AjaxResult changeStatus(@RequestBody AppNews appNews) {
         return toAjax(appNewsService.changeNewsStatus(appNews));
+    }
+
+    @ApiOperation("新闻删除")
+    @PreAuthorize("@ss.hasPermi('app:news:remove')")
+    @Log(title = "新闻管理", businessType = BusinessType.DELETE)
+    @DeleteMapping("/{newsIds}")
+    public AjaxResult remove(@PathVariable String[] newsIds) {
+        return toAjax(appNewsService.removeNewsByIds(newsIds));
+    }
+
+    @ApiOperation("新闻修改")
+    @PreAuthorize("@ss.hasPermi('app:news:edit')")
+    @Log(title = "新闻管理", businessType = BusinessType.UPDATE)
+    @PutMapping()
+    public AjaxResult edit(@RequestBody AppNews appNews) {
+        return toAjax(appNewsService.editNews(appNews));
     }
 
 }
