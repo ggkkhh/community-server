d933e00e8eca1998b16e9353dd175eff0b87d210
roydonGuo <3133010060@qq.com>
2023-06-13 13:03:18 +0800

    新闻类型修改

--- community-business/community-business-news/src/main/java/com/roydon/business/news/enums/NewsType.java
+++ community-business/community-business-news/src/main/java/com/roydon/business/news/enums/NewsType.java
@@ -13,15 +13,11 @@ import java.util.Map;
  **/
 public enum NewsType {
 
-    FINANCE("509", "财经"),
     SCIENCE("510", "科技"),
     MILITARY("511", "军事"),
     FASHION("512", "时尚"),
-    LOF("514", "股票"),
     HEALTH("516", "健康"),
     REVIEW("518", "要闻"),
-    SPORT("519", "体育"),
-    ENTERTAINMENT("520", "娱乐"),
     HEADLINE("521", "头条"),
     HOTSPOT("525", "热点");
 
