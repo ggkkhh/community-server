2b3a3dcaa6133027c5485fc5f438d67bc3575a6a
roydon <3133010060@qq.com>
2023-07-17 23:30:55 +0800

    缓存热点新闻定时任务

--- community-component/community-component-quartz/src/main/java/com/roydon/quartz/task/NewsTask.java
+++ community-component/community-component-quartz/src/main/java/com/roydon/quartz/task/NewsTask.java
@@ -1,6 +1,7 @@
 package com.roydon.quartz.task;
 
 import com.roydon.business.news.httpclient.GetNewsService;
+import com.roydon.business.news.service.AppNewsService;
 import com.roydon.business.news.task.UpdateNewsViewNumTask;
 import org.springframework.stereotype.Component;
 
@@ -19,13 +20,29 @@ public class NewsTask {
     private GetNewsService getNewsService;
 
     @Resource
+    private AppNewsService appNewsService;
+
+    @Resource
     private UpdateNewsViewNumTask updateNewsViewNumTask;
 
+    /**
+     * 定时拉取第三方新闻
+     */
     public void updateNewsDaily() {
         getNewsService.getNewsList();
     }
 
+    /**
+     * 更新新闻浏览量
+     */
     public void updateNewsViewNum() {
         updateNewsViewNumTask.updateViewCount();
+    }
+
+    /**
+     * 缓存热点新闻
+     */
+    public void setHotNewsToCache() {
+        appNewsService.setHotNewsToCache();
     }
 }
