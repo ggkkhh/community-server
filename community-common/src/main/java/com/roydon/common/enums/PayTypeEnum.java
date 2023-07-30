b831a47f071e3c6ab763e7e7993c04c663bf60d5
roydon <3133010060@qq.com>
2023-07-30 14:30:42 +0800

    支付类型枚举

---
+++ community-common/src/main/java/com/roydon/common/enums/PayTypeEnum.java
@@ -0,0 +1,36 @@
+package com.roydon.common.enums;
+
+/**
+ * 支付方式枚举
+ */
+public enum PayTypeEnum {
+    // 微信
+    WX(1),
+    // 支付宝
+    ALI(2),
+    // 测试
+    ALI_TEST(3);
+
+    private int code;
+
+    PayTypeEnum(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static PayTypeEnum of(int code) {
+        for (PayTypeEnum value : PayTypeEnum.values()) {
+            if (code == value.getCode()) {
+                return value;
+            }
+        }
+        return null;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+}
