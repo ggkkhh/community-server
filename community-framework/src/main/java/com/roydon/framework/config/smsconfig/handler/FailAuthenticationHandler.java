647116def21a5cd29dc8cf207fa2d01898d4e58d
roydonGuo <3133010060@qq.com>
2023-09-27 14:04:48 +0800

    security认证失败处理器

--- community-framework/src/main/java/com/roydon/framework/config/smsconfig/handler/FailAuthenticationHandler.java
+++ community-framework/src/main/java/com/roydon/framework/config/smsconfig/handler/FailAuthenticationHandler.java
@@ -7,11 +7,11 @@ import com.roydon.common.core.domain.entity.SysUser;
 import com.roydon.framework.manager.AsyncManager;
 import com.roydon.framework.manager.factory.AsyncFactory;
 import com.roydon.system.service.ISysUserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
- 
+
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,14 +19,12 @@ import java.io.IOException;
 
 @Component
 public class FailAuthenticationHandler extends SimpleUrlAuthenticationFailureHandler {
- 
-    @Autowired
+
+    @Resource
     private ISysUserService userService;
- 
+
     @Override
-    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-                                        AuthenticationException exception) throws IOException, ServletException {
- 
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         String mobile = request.getParameter("telephone");
         SysUser sysUser = userService.getUserByTelephone(mobile);
         if (sysUser == null) {
@@ -34,13 +32,13 @@ public class FailAuthenticationHandler extends SimpleUrlAuthenticationFailureHan
         } else {
             AsyncManager.me().execute(AsyncFactory.recordLogininfor(sysUser.getUserName(), Constants.LOGIN_FAIL, exception.getMessage()));
         }
- 
+
         JSONObject res = new JSONObject();//返回前端数据
         res.put("code", HttpStatus.ERROR);
         res.put("msg", exception.getMessage());
         response.setContentType("application/json;charset=UTF-8");
         response.getWriter().write(String.valueOf(res));
- 
+
     }
- 
+
 }
